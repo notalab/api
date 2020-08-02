e677f756a9f107fce7b1522a935c5aadbd52c582
Mitch <me@miitch.io>
2020-08-02 22:40:48 +0800

    Staging & deploy attempt 1 CI

---
+++ deploy-staging.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Pulling latest version from staging"
+git fetch --all && git checkout --force "origin/staging"
+git pull
+cp .env.example .env
+
+echo "Installing server dependencies"
+docker-compose run --rm server pip install -r requirements-dev.txt --user --upgrade --no-warn-script-location
+
+echo "Upgrading database"
+docker-compose run --rm server python src/manage.py db upgrade
+
+echo "Restarting daemon"
+docker-compose stop
+docker-compose up -d server
+
+echo "Finished"
