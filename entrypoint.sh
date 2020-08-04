2649e603e8379aa4e909d0094f635f8351a84c91
mtchdev <fsmitchm@gmail.com>
2020-08-04 15:23:32 +0800

    more scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 #flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - --chdir ./src wsgi:application
+exec gunicorn -b :5000 \
+    --access-logfile - \
+    --error-logfile - \
+    --chdir ./src \
+    wsgi:application
--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-docker run --name nota -d -p 8000:5000 nota:latest
+#/bin/bash
+docker run --name nota -d -p 8000:5000 nota:latest \
+    -e "APPLICATION_ROOT=/api" \
+    -e "APPLICATION_POSTGRES_HOST=127.0.0.1" \
+    -e "APPLICATION_POSTGRES_USER=docker" \
+    -e "APPLICATION_POSTGRES_PW=docker" \
+    -e "APPLICATION_POSTGRES_PORT=32772" \
+    -e "APPLICATION_POSTGRES_DB=docker" \
+    -e "ENVIRONMENT=dev"
