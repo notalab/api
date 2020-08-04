8b6f660a5c2fa10b5f402a0225fb5f179a21e62f
Mitch <me@miitch.io>
2020-08-04 21:12:30 +0800

    Fix tests

--- test/test_user.py
+++ test/test_user.py
@@ -4,7 +4,7 @@ import unittest
 from models import User
 from models.abc import db
 from repositories import UserRepository
-from server import server
+from app import server
 
 
 class TestUser(unittest.TestCase):
